ad11def38a0f3585a04d6e3373b511b5f47e253d
Anton_PC <Anton@PC.com>
2017-07-12 17:08:19 +0200

    Starting Relic support

--- Athenas ASS to Android app converter.py
+++ Athenas ASS to Android app converter.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-charms = open("mycharms.txt", 'r', encoding='UTF-8', newline=None)
-c = charms.read()
-c = c.splitlines()
-translate = open('Skilllist Eng-JP.csv', 'r', encoding='UTF-8')
-read_data = translate.read()
-read_data = read_data.splitlines()
-read_data[0] = 'Amplify;増幅'
 
 
-def return_charm_values(num):
+def return_charm_values(num, c, read_charms_data):
     line = c[num+1]
     line = line.split(',')
     slots = line[0]
@@ -19,7 +12,7 @@ def return_charm_values(num):
     points2 = line[4]
     if not points2:
         points2 = 0
-    for i in read_data:
+    for i in read_charms_data:
         if i.split(';')[0] == skill1:
             skill1 = i.split(';')[1]
         elif i.split(';')[0] == skill2:
@@ -28,12 +21,20 @@ def return_charm_values(num):
     return line
 
 
-def magic():
+#def return_relic_values(num, r, read_relic_data):
+
+
+def charms_convert():
     conn = sqlite3.connect('charms.db')
     db = conn.cursor()
     db.execute('DELETE FROM charms;')
+    charms = open("mycharms.txt", 'r', encoding='UTF-8', newline=None)
+    c = charms.read().splitlines()
+    translate = open('Skilllist Eng-JP.csv', 'r', encoding='UTF-8')
+    read_charms_data = translate.read().splitlines()
+    read_charms_data[0] = 'Amplify;増幅'
     for i in range(len(c)-1):
-        athenas_values = return_charm_values(i)
+        athenas_values = return_charm_values(i, c, read_charms_data)
         line = 'INSERT INTO charms VALUES ('\
                + str(athenas_values[0])\
                + ',\"'\
@@ -52,4 +53,4 @@ def magic():
     conn.close()
     charms.close()
     return None
-magic()
+charms_convert()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a script to convert the files containing the charms of a player in [Athe
 ***ROOT REQUIRED***
 
 ## How to use
-Clone the repo into a folder and put the `mycharms.txt`file from Athenas ASS and the `charms.db` file from Hunter’s Simulator for MH4U into the same folder. Run the script with Python 3 and you're done.
+Clone the repo into a folder and put the `mycharms.txt` file from the data folder from the folder where the Athenas ASS exe is stored and the `charms.db` file from Hunter’s Simulator for MH4U (`/data/data/com.thisisgame.mhsimul/databases/`) into the same folder. Run the script with Python 3 and put the edited `charms.db` back on your phone to `/data/data/com.thisisgame.mhsimul/databases/`.
 
 #### To-Do
 - [ ] Add support for relic weapons
