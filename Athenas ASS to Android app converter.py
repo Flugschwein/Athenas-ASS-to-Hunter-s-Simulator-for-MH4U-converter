df393e96287ccbc9b24f3c0a4491310c7468ab3f
Anton_PC <Anton@PC.com>
2017-07-17 22:40:09 +0200

    It's getting further

--- Athenas ASS to Android app converter.py
+++ Athenas ASS to Android app converter.py
@@ -2,17 +2,13 @@
 import sqlite3
 
 
-def return_charm_values(num, c, read_charms_data):
+def return_charm_values(num, c, read_translation_data):
     line = c[num+1]
     line = line.split(',')
-    slots = line[0]
-    skill1 = line[1]
-    points1 = line[2]
-    skill2 = line[3]
-    points2 = line[4]
+    slots, skill1, points1, skill2, points2, = line
     if not points2:
         points2 = 0
-    for i in read_charms_data:
+    for i in read_translation_data:
         if i.split(';')[0] == skill1:
             skill1 = i.split(';')[1]
         elif i.split(';')[0] == skill2:
@@ -21,14 +17,27 @@ def return_charm_values(num, c, read_charms_data):
     return line
 
 
-def return_relic_values(num, r, read_relic_data):
-    # Format: armor slot,  Blademaster/Gunner, Points, Skill, Defense, Fire Res, Water res, Ice Res, Thndr Res, Drgn Res
+def return_relic_values(num, r, read_translation_data):
+    # Format: itemType,  Blademaster/Gunner, Points, Skill, Defense, Fire Res, Water res, Ice Res, Thndr Res, Drgn Res
     line = r[num]
     line.split(',')
     if len(line) == 5:
+        item_type, style, points, skill, defence = line
+        item_type = convert_item_type(item_type)
+        style = convert_hunterType(style)
 
         line = [num+1, ]
     return line
+
+
+def convert_hunter_type(num):
+    numbers = {1:0, 2:1 3:1}
+    return numbers[num]
+
+
+def convert_item_type(num):
+    slot_numbers = {0:0, 1:3, 2:2, 3:1, 4:4, 5:6}
+    return slot_numbers[num]
 
 
 def charms_convert():
@@ -38,10 +47,10 @@ def charms_convert():
     charms = open("mycharms.txt", 'r', encoding='UTF-8', newline=None)
     c = charms.read().splitlines()
     translate = open('Skilllist Eng-JP.csv', 'r', encoding='UTF-8')
-    read_charms_data = translate.read().splitlines()
-    read_charms_data[0] = 'Amplify;増幅'
+    read_translation_data = translate.read().splitlines()
+    read_translation_data[0] = 'Amplify;増幅'
     for i in range(len(c)-1):
-        athenas_values = return_charm_values(i, c, read_charms_data)
+        athenas_values = return_charm_values(i, c, read_translation_data)
         line = 'INSERT INTO charms VALUES ('\
                + str(athenas_values[0])\
                + ',\"'\
